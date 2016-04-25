2b68d86476562bd81a980347d5bb13baf980bca4
Nataliia <sumovskaya@gmail.com>
2016-04-25 16:36:23 +0700

    initial build

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+/*jslint nomen: true, regexp: true, unparam: true, sloppy: true, white: true, node: true */
+/*global window, console, document, $, jQuery, google */
+
+/**
+ * On document ready
+ */
+$(document).ready(function () {
+
+	/** Fastclick */
+	FastClick.attach(document.body);
+
+	/*** Forms ***/
+	initForms();
+
+});
+
+/**
+ * Forms initialization
+ */
+function initForms(scope) {
+	if (typeof scope === 'undefined') {
+		scope = document;
+	}
+}
+
+/**
+ * Magnific Popup default settings
+ */
+$.extend(true, $.magnificPopup.defaults, {
+	tClose: 'Закрыть (Esc)',
+	tLoading: '',
+	closeMarkup: '<span title="%title%" class="mfp-close"><span class="mfp-in"></span></span>',
+	ajax: {tError: '<a href="%url%">Контент</a> не найден.'},
+	settings: {cache: false},
+	mainClass: 'mfp-zoom-in',
+	midClick: true,
+	removalDelay: 300,
+	autoFocusLast: false,
+	preload: false,
+	callbacks: {
+		ajaxContentAdded: function () {
+			initForms(this.container);
+		}
+	}
+});
